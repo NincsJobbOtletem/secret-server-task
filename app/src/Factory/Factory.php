743b522ca7c80e6290676597f0707d40f860a7bd
NincsJobbOtletem <patrikvok123312@gmail.com>
2022-07-29 15:55:55 +0200

    nothing

--- app/src/Factory/Factory.php
+++ app/src/Factory/Factory.php
@@ -8,23 +8,5 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class Factory
 {
-    // public function __construct(private SerializerInterface $serializer)
-    // {
-    // }
- 
-    // public function create(object $data, string $xmlRoot, int $status = 200, array $headers = []): Response
-    // {
-    //     return new Response(
-    //         $this->serializer->serialize(
-    //             $data,
-    //             XmlEncoder::FORMAT,
-    //             [
-    //                 XmlEncoder::ROOT_NODE_NAME => $xmlRoot,
-    //                 XmlEncoder::ENCODING => 'UTF-8',
-    //             ]
-    //         ),
-    //         $status,
-    //         array_merge($headers, ['Content-Type' => 'application/xml;charset=UTF-8'])
-    //     );
-    // }
+   
 }
